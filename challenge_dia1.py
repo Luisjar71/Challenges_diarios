d2ac0a510ff9136a5d6d7b2ab8d5564709bbc348
Luisjar71 <125704446+Luisjar71@users.noreply.github.com>
2024-06-12 11:36:26 -0400

    Add files via upload

---
+++ challenge_dia1.py
@@ -0,0 +1,9 @@
+def invertir_caracteres(cadena_de_caracteres):
+    if len(cadena_de_caracteres) == 0:
+        return ""
+    
+    else:
+        return cadena_de_caracteres[-1] + invertir_caracteres(cadena_de_caracteres[:-1])
+    
+resultado = invertir_caracteres("Manchester United")
+print(resultado)
