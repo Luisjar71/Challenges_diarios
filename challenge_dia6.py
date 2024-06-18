62708caab0c3fc3d0ec257351665edd12b339bf2
Luisjar71 <125704446+Luisjar71@users.noreply.github.com>
2024-06-18 11:53:17 -0400

    Add files via upload

---
+++ challenge_dia6.py
@@ -0,0 +1,7 @@
+# Conversión de Temperatura: Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit.
+
+temperatura = float(input("Ingrese una temperatura Celcius: "))
+
+tem_faren = temperatura * (9/5) + 32
+
+print(f"La temperatura es {tem_faren} Fahrenheit")
